0b28d233059c360de88f65859b92b77183978875
“xiezeyi” <xiezy@miligc.com>
2021-07-22 15:15:06 +0800

    feat: y21合并两个有序链表

---
+++ leetcode/src/x2021/z07/Node.java
@@ -0,0 +1,19 @@
+package x2021.z07;
+
+/**
+ * @program: Algorithm
+ * @description: y22
+ * @author: 谢泽毅
+ * @create: 2021-07-22 14:21
+ **/
+public class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
