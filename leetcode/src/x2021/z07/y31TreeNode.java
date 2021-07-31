8d31a07d0a7fd419963c62b11bcd408b4dd063fc
xzy <394337123@qq.com>
2021-07-31 13:27:20 +0800

    feat: 二叉树的垂序遍历

---
+++ leetcode/src/x2021/z07/y31TreeNode.java
@@ -0,0 +1,18 @@
+package x2021.z07;
+
+/**
+ * @author 谢泽毅
+ * @data 2021/7/31 13:22
+ */
+public class y31TreeNode {
+    int val;
+    y31TreeNode left;
+    y31TreeNode right;
+    y31TreeNode() {}
+    y31TreeNode(int val) { this.val = val; }
+    y31TreeNode(int val, y31TreeNode left, y31TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+    }
+}
---
+++ leetcode/src/x2021/z07/y31二叉树的垂序遍历.java
@@ -0,0 +1,54 @@
+package x2021.z07;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * 二叉树的垂序遍历
+ * @author 谢泽毅
+ * @data 2021/7/31 11:20
+ */
+public class y31二叉树的垂序遍历 {
+    public static void main(String[] args) {
+
+    }
+    public List<List<Integer>> verticalTraversal(y31TreeNode root) {
+        List<int[]> nodes = new ArrayList<int[]>();
+        dfs(root, 0, 0, nodes);
+        Collections.sort(nodes, new Comparator<int[]>() {
+            public int compare(int[] tuple1, int[] tuple2) {
+                if (tuple1[0] != tuple2[0]) {
+                    return tuple1[0] - tuple2[0];
+                } else if (tuple1[1] != tuple2[1]) {
+                    return tuple1[1] - tuple2[1];
+                } else {
+                    return tuple1[2] - tuple2[2];
+                }
+            }
+        });
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        int size = 0;
+        int lastcol = Integer.MIN_VALUE;
+        for (int[] tuple : nodes) {
+            int col = tuple[0], row = tuple[1], value = tuple[2];
+            if (col != lastcol) {
+                lastcol = col;
+                ans.add(new ArrayList<Integer>());
+                size++;
+            }
+            ans.get(size - 1).add(value);
+        }
+        return ans;
+    }
+    public void dfs(y31TreeNode node, int row, int col, List<int[]> nodes) {
+        if (node == null) {
+            return;
+        }
+        nodes.add(new int[]{col, row, node.val});
+        dfs(node.left, row + 1, col - 1, nodes);
+        dfs(node.right, row + 1, col + 1, nodes);
+    }
+
+}
